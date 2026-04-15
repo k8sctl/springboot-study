5da55ef1d56bc5f18b3c9460578922b077bb8bbb
thbins <joons0218@gmail.com>
2026-04-15 13:42:52 +0900

    학습: 사용자 조회·등록·삭제 API와 HTTP 상태 코드 및 예외 처리 구현

--- src/main/java/com/example/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/com/example/myrestfulservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.myrestfulservice.controller;
 
 import com.example.myrestfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World Bean!");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
---
+++ src/main/java/com/example/myrestfulservice/dao/UserDaoService.java
@@ -0,0 +1,66 @@
+package com.example.myrestfulservice.dao;
+
+import com.example.myrestfulservice.bean.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static List<User> users = new ArrayList<>();
+
+    private static int userCount = 3;
+
+    static {
+        users.add(new User(1, "Yuki", new Date()));
+        users.add(new User(2, "Alice", new Date()));
+        users.add(new User(3, "Anna", new Date()));
+    }
+
+    // 사용자 전체 조회
+    public  List<User> findAll() {
+        return users;
+    }
+
+    // 사용자 저장하기
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++userCount);
+        }
+
+        if (user.getJoinDate() == null) {
+            user.setJoinDate(new Date());
+        }
+
+        users.add(user);
+
+        return user;
+    }
+
+    // Id로 사용자 단일 조회
+    public User findOne(int id) {
+        for (User user : users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User deleteById(int id) {
+        Iterator<User> iterator = users.iterator();
+
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+
+            if (user.getId() == id) {
+                iterator.remove();
+                return user;
+            }
+        }
+        return null;
+    }
+}
