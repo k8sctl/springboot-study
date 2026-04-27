eea3a7e917774cd64e07f00324774fbc3d087fc9
thbins <joons0218@gmail.com>
2026-04-27 14:47:27 +0900

    feat: Admin API 응답 필드 동적 필터링 기능 구현
    - AdminUserController 추가 (/admin/users/{id} 엔드포인트)
    - AdminUser 빈 추가 및 @JsonFilter(UserInfo) 적용
    - SimpleBeanPropertyFilter로 id, name, joinDate, ssn 필드만 노출
    - BeanUtils.copyProperties를 통해 User -> AdminUser 데이터 복사

---
+++ src/main/java/com/example/myrestfulservice/controller/AdminUserController.java
@@ -0,0 +1,49 @@
+package com.example.myrestfulservice.controller;
+
+import com.example.myrestfulservice.bean.AdminUser;
+import com.example.myrestfulservice.bean.User;
+import com.example.myrestfulservice.dao.UserDaoService;
+import com.example.myrestfulservice.exception.UserNotFoundException;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+    private UserDaoService service;
+
+    public AdminUserController(UserDaoService service) {
+        this.service = service;
+    }
+
+    // /admin/users/{id}
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser4Admin(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        AdminUser adminUser = new AdminUser();
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        } else {
+            BeanUtils.copyProperties(user, adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo",filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+}
